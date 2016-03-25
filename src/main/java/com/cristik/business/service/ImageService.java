7dbc532fafd91342948a5698e3f6d4ecca3421ee
cristik1991 <cristik1991@gmail.com>
2016-03-25 15:09:51 +0800

    图片业务层

---
+++ src/main/java/com/cristik/business/service/ImageService.java
@@ -0,0 +1,15 @@
+package com.cristik.business.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by zhenghua on 2016/3/24.
+ */
+
+@Service
+public class ImageService {
+    public String saveImage(MultipartFile file){
+        return "";
+    }
+}
