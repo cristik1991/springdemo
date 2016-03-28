c869185e83a84729026eee430627cca0cdb5bda2
cristik1991 <cristik1991@gmail.com>
2016-03-28 10:07:15 +0800

    adminlte 测试页面

--- src/main/java/com/cristik/business/controller/MainController.java
+++ src/main/java/com/cristik/business/controller/MainController.java
@@ -7,13 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Created by zhenghua on 2016/3/11.
  */
+
 @Controller
+@RequestMapping("/main")
 public class MainController {
-	@Autowired
+
+
 	@RequestMapping("/")
 	public String index(){
 		System.out.println("here is the welcome page");
 		System.out.println(111);
 		return "index";
+	}
+
+	@RequestMapping("/test")
+	public String test(){
+		return "/test/test";
 	}
 }
