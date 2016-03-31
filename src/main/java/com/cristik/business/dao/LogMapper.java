c4c66cc3c13636d2a8dfafcf70997772cb8bfff0
cristik1991 <cristik1991@gmail.com>
2016-03-31 15:28:28 +0800

    删除原日志文件

--- src/main/java/com/cristik/business/dao/LogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cristik.business.dao;
-
-import com.cristik.business.entity.svo.Log;
-import java.util.List;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface LogMapper extends Mapper<Log> {
-
-}
